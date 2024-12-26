b675495c0a8173bdcf7cf369bcdaed0adba83d3a
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2024-12-26 18:33:09 +0200

    create seed to film and genre

--- app/Models/Films.php
+++ app/Models/Films.php
@@ -16,4 +16,21 @@ class Films extends Model
         'status_published',
         'link_poster'
     ];
+
+    public function genres(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Genres::class, 'film_genre', 'fk_film', 'fk_genre');
+    }
+
+//    protected static function boot()
+//    {
+//        static::created(function ($model) {
+//            $randomGenre = Genres::inRandomOrder()->first();
+//            if ($randomGenre) {
+//                $model->genres()->attach($randomGenre->id);
+//            }
+//        });
+//
+//        parent::boot();
+//    }
 }
